24a9c2fc92a1bb3d5c38f5716451e0cd430e8d6d
Ophéline Wattier <opheliiinee@gmail.com>
2025-09-01 14:57:11 +0200

    maj page d'inscription ok

--- src/Pages/RegisterPage.jsx
+++ src/Pages/RegisterPage.jsx
@@ -3,34 +3,57 @@ import { register } from "../Services/userServices";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Card from "react-bootstrap/Card";
-import Toast from "react-bootstrap/Toast";
-import ToastContainer from "react-bootstrap/ToastContainer";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+
+import "../Styles/registerPage.css"
 
 const RegisterPage = () => {
   const [userData, setUserData] = useState({
     username: "",
     mail: "",
     password: "",
+    verify_password: "",
   });
 
-  const [showToast, setShowToast] = useState(false);
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
+      if (
+        !userData.username ||
+        !userData.mail ||
+        !userData.password ||
+        !userData.verify_password
+      ) {
+        toast.error("Tous les champs sont obligatoires");
+        return;
+      }
+
+      if (userData.password !== userData.verify_password) {
+        toast.error("Les mots de passe ne correspondent pas");
+        return;
+      }
+      if (
+        RegExp(
+          /^(?=.{1,254}$)(?=.{1,64}@)(?!.*\.\.)[\p{L}\p{N}!#$%&'*+/=?^_`{|}~-]+(?:\.[\p{L}\p{N}!#$%&'*+/=?^_`{|}~-]+)*@(?:(?!-)[\p{L}\p{N}-]{1,63}(?<!-)\.)+[\p{L}]{2,63}$/u
+        ).test(userData.mail) === false
+      ) {
+        toast.error("L'email n'est pas valide");
+        return;
+      }
+
       await register(userData);
       toast.success("enregistrement réussi");
-        navigate("/login");
+      navigate("/login");
     } catch (error) {
       console.error(error);
     }
   };
 
   return (
-    <div className="d-flex flex-column align-items-center justify-content-center gap-3">
+    <div className="register-page">
       <h1>Inscription :</h1>
       <Card style={{ width: "30rem" }}>
         <Card.Body className="d-flex flex-column align-items-center justify-content-center gap-3">
@@ -74,6 +97,20 @@ const RegisterPage = () => {
               />
             </Form.Group>
 
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Confirmation de votre mot de passe :</Form.Label>
+              <Form.Control
+                name="verify_password"
+                type="password"
+                placeholder="Entrer votre mot de passe"
+                required
+                value={userData.verify_password}
+                onChange={(e) =>
+                  setUserData({ ...userData, verify_password: e.target.value })
+                }
+              />
+            </Form.Group>
+
             <div className="d-flex justify-content-center">
               <Button variant="primary" type="submit">
                 S'inscrire
@@ -90,4 +127,3 @@ const RegisterPage = () => {
 };
 
 export default RegisterPage;
-
